23a270e787e954f93cb90cf82522a97f164abf4c
James Otipa <otipajames2@gmail.com>
2026-09-08 10:46:12 +0300

    feat: add save add-savings route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-﻿import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
@@ -10,6 +10,7 @@ import LipaNaAirSave from "./pages/LipaNaAirSave.jsx";
 import Paybill from "./pages/Paybill.jsx";
 import Goals from "./pages/Goals.jsx";
 import GoalNew from "./pages/GoalNew.jsx";
+import AddSavings from "./pages/AddSavings.jsx";
 import Transactions from "./pages/Transactions.jsx";
 import Withdraw from "./pages/Withdraw.jsx";
 import Wallet from "./pages/Wallet.jsx";
@@ -31,149 +32,29 @@ function App() {
             <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/wallet"
-              element={
-                <ProtectedRoute>
-                  <Wallet />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/wallet/buy-goods"
-              element={<Navigate to="/lipa-na-airsave" replace />}
-            />
-            <Route
-              path="/payments"
-              element={
-                <ProtectedRoute>
-                  <Payments />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/deposit"
-              element={
-                <ProtectedRoute>
-                  <Deposit />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/save"
-              element={
-                <ProtectedRoute>
-                  <Goals />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/save/create"
-              element={
-                <ProtectedRoute>
-                  <GoalNew />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/save/create-goal"
-              element={<Navigate to="/save/create" replace />}
-            />
-            <Route
-              path="/send"
-              element={
-                <ProtectedRoute>
-                  <Send />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/lipa-na-airsave"
-              element={
-                <ProtectedRoute>
-                  <LipaNaAirSave />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/payments/buy-goods"
-              element={<Navigate to="/lipa-na-airsave" replace />}
-            />
-            <Route
-              path="/payments/paybill"
-              element={
-                <ProtectedRoute>
-                  <Paybill />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/goals"
-              element={<Navigate to="/save" replace />}
-            />
-            <Route
-              path="/goals/new"
-              element={<Navigate to="/save/create" replace />}
-            />
-            <Route
-              path="/activity"
-              element={
-                <ProtectedRoute>
-                  <Transactions />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/withdraw"
-              element={
-                <ProtectedRoute>
-                  <Withdraw />
-                </ProtectedRoute>
-              }
-            />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+            <Route path="/wallet" element={<ProtectedRoute><Wallet /></ProtectedRoute>} />
+            <Route path="/wallet/buy-goods" element={<Navigate to="/lipa-na-airsave" replace />} />
+            <Route path="/payments" element={<ProtectedRoute><Payments /></ProtectedRoute>} />
+            <Route path="/deposit" element={<ProtectedRoute><Deposit /></ProtectedRoute>} />
+            <Route path="/save" element={<ProtectedRoute><Goals /></ProtectedRoute>} />
+            <Route path="/save/create" element={<ProtectedRoute><GoalNew /></ProtectedRoute>} />
+            <Route path="/save/add" element={<ProtectedRoute><AddSavings /></ProtectedRoute>} />
+            <Route path="/save/create-goal" element={<Navigate to="/save/create" replace />} />
+            <Route path="/send" element={<ProtectedRoute><Send /></ProtectedRoute>} />
+            <Route path="/lipa-na-airsave" element={<ProtectedRoute><LipaNaAirSave /></ProtectedRoute>} />
+            <Route path="/payments/buy-goods" element={<Navigate to="/lipa-na-airsave" replace />} />
+            <Route path="/payments/paybill" element={<ProtectedRoute><Paybill /></ProtectedRoute>} />
+            <Route path="/goals" element={<Navigate to="/save" replace />} />
+            <Route path="/goals/new" element={<Navigate to="/save/create" replace />} />
+            <Route path="/activity" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
+            <Route path="/withdraw" element={<ProtectedRoute><Withdraw /></ProtectedRoute>} />
             <Route path="/savings" element={<Navigate to="/save" replace />} />
             <Route path="/transactions" element={<Navigate to="/activity" replace />} />
-            <Route
-              path="/settings"
-              element={
-                <ProtectedRoute>
-                  <Settings />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <Profile />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <Admin />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/support"
-              element={
-                <ProtectedRoute>
-                  <Support />
-                </ProtectedRoute>
-              }
-            />
+            <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+            <Route path="/support" element={<ProtectedRoute><Support /></ProtectedRoute>} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
@@ -184,4 +65,3 @@ function App() {
 }
 
 export default App;
-
